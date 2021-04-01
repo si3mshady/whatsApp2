60b3075760d3d76fb9a7cd41a2d8e063f676decc
si3mshady <cloudwarrior2891@gmail.com>
2021-04-01 14:25:27 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+       <h1>Let's build Whatsapp 2.0</h1>
+    </div>
+  )
+}
